ea20abcf752d0954d1c5c48409d9b80561f54b83
lurk101 <lurkone0one@gmail.com>
2023-04-16 01:51:02 -0400

    use the PLL

--- main.c
+++ main.c
@@ -4,20 +4,12 @@
 #include <stdlib.h>
 
 int main() {
-    // set system clock to 90.39548 MHz, this will give
-    // closest to 44.1KHz * 64 for 32X oversampling.
-    // actual ends up at 44.13841797 KHz
-    const uint32_t f = 90395480;
-    clock_configure(clk_sys, CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
-                    CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, 125000000, f);
-    uint32_t a = clock_get_hz(clk_sys);
+    // set system clock to 96 MHz, this will give
+    // divided by 34 gives 2.823529 MHz for I2S clk (44100 * 16 * 2 * 2 = 2,822,400)
+    // and divided by 68 gives 1.411764 MHz PCM clk (44100 * 32 = 1,411,200)
     stdio_init_all();
     getchar_timeout_us(250000);
     // clear the screen on VT terminal
-    printf("\033[H\033[J");
-    if (a != f) {
-        printf("clk mismatch\n");
-        exit(-1);
-    }
-    printf("sys clk set to %d Hz.\n", clock_get_hz(clk_sys));
+    printf("\033[H\033[JPico I2S PDM Stereo DAC\n");
+    printf("System clock %f MHz\n", clock_get_hz(clk_sys) / 1.0e6);
 }
